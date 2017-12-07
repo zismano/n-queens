ec77f46312b39cef18ad136db80e142e658e65d9
zismano <zismanofir@gmail.com>
2017-12-07 11:14:16 -0800

    Implement a function which counts all n-queens solutions

--- src/solvers.js
+++ src/solvers.js
@@ -107,7 +107,27 @@ window.findNQueensSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0; //fixme
+  var solution = []; 
+  var board = new Board({n: n});
+  if (n !== 2 && n !== 3) {
+    var renderNQueensSolution = function(r) {
+      if (r === n) {
+        //the base case puts a solution into the solution
+        solutionCount++;
+        return;
+      }
+      for (var i = 0; i < n; i++) { 
+        board.togglePiece(r, i);
+        //if there are no conflicts, recurse
+        if (!board.hasAnyColConflicts() && !board.hasAnyMajorDiagonalConflicts() && !board.hasAnyMinorDiagonalConflicts()) {
+          renderNQueensSolution(r + 1);
+        } 
+        board.togglePiece(r, i);
+      }
+    };
+    renderNQueensSolution(0);
+  }
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
